36f907d145cb2d512a89416fbdad99965a8d5a1f
JeanM-Pro <jeancenteno54@gmail.com>
2021-10-19 07:00:12 -0500

    se configuro materialize para las clases

--- src/RelojProject.js
+++ src/RelojProject.js
@@ -1,47 +1,24 @@
 import React, {useState, useEffect} from "react"
 
 export const RelojProject = () => {
-    const [minutos, setMinutos] = useState(0)
-    const [segundos, setSegundos] = useState(5)
-    const [descanso, setDescanso] = useState(true)
+   
+    const [minutos, setMinutos] = useState(25);
+    const [segundos, setSegundos] = useState(20);
 
-    useEffect(()=>{
     
-        let interval = setInterval(() => {
-            clearInterval(interval)
-            if(minutos === 0 && segundos === 0) {
-                setDescanso(!descanso)
-                if(descanso) {
-                    setMinutos(5)
-                    setSegundos(0)
-                } else {
-                    setMinutos(25)
-                    setSegundos(0)
-                    setDescanso(false)
-                }
-            }
-            if(segundos === 0) {
-                if(minutos !== 0) {
-                    setSegundos(59)
-                    setMinutos(minutos -1)
-                }
-                else {
-                    
-                }
-            } else setSegundos(segundos -1)
-            
-        }, 1000)
-    }, [minutos, segundos, descanso])
 
-    const formatearMinutos = minutos < 10 ? `0${minutos}`: minutos
-    const formatearSegundos = segundos < 10 ? `0${segundos}`: segundos
+
+
+    const showMinutos = minutos < 10 ? `0${minutos}` : minutos;
+    const showSegundos = segundos < 10 ? `0${segundos}` : segundos;
+
 
     return (
-        <div>
-            <h1>{descanso ? 'Hora de descansar' : 'Hora de Trabajar'}</h1>
-            <h1>{formatearMinutos}:{formatearSegundos}</h1>
-            <button onClick={() => setMinutos(minutos +1)}>+</button>
-            <button onClick={() => setMinutos(minutos -1)}>-</button>
-        </div>
+        <>
+            <h1>{showMinutos}:{showSegundos}</h1>
+            <button onClick={()=>{setMinutos(minutos + 1)}}>+</button>
+            <button onClick={()=>{setMinutos(minutos - 1)}}>-</button>
+            {/* <button onClick={()=>{play()}}>play</button> */}
+        </>
     )
 }
